4d9ebebd11be2467789a7a34c0f989f7b2a37483
jjeankim <miyukikj35@gmail.com>
2025-07-01 20:50:54 +0900

    Feat:log 업데이트&삭제 구현

--- src/controllers/logController.ts
+++ src/controllers/logController.ts
@@ -25,8 +25,8 @@ export const createLog = async (req: UserRequest, res: Response) => {
 
 export const getLogs: RequestHandler = async (req, res) => {
   const logs = await prisma.log.findMany({
-    where: {isPublic: true,},
-    orderBy:{createdAt: "desc"}
+    where: { isPublic: true },
+    orderBy: { createdAt: "desc" },
   });
   res.status(200).json({ message: "log 목록 가져오기 성공", data: logs });
 };
@@ -36,9 +36,54 @@ export const getLog: RequestHandler = async (req, res) => {
   const log = await prisma.log.findFirst({
     where: { id, isPublic: true },
   });
-  if(!log) {
-    res.status(404).json({message:"해당 공개 로그를 찾을 수 없습니다."})
+  if (!log) {
+    res.status(404).json({ message: "해당 공개 로그를 찾을 수 없습니다." });
   }
-  
+
   res.status(200).json({ message: "ok", data: log });
+};
+
+export const updateLog = async (req: UserRequest, res: Response) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+
+  const id = Number(req.params.id);
+  const log = await prisma.log.findUnique({
+    where: { id },
+  });
+  if (!log) {
+    res.status(404).json({ message: "해당 로그를 찾을 수 없습니다." });
+  }
+
+  if (req.user.id !== log?.userId) {
+    res.status(404).json({ message: "수정 권한이 없습니다." });
+  }
+
+  const { title, content, bookAuthor, bookImgUrl, rating } = req.body;
+  const updatedLog = await prisma.log.update({
+    where: { id },
+    data: {
+      title,
+      content,
+      bookAuthor,
+      bookImgUrl,
+      rating,
+    },
+  });
+  res.status(200).json({ message: "로그 수정 성공", data: updatedLog });
+};
+
+export const deleteLog = async (req: UserRequest, res: Response) => {
+  const id = Number(req.params.id);
+  try {
+    const log = await prisma.log.delete({
+      where: { id },
+    });
+
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: "서버 오류", error });
+  }
 };
--- src/routes/logRouter.ts
+++ src/routes/logRouter.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import { createLog, getLog, getLogs } from "../controllers/logController";
+import {
+  createLog,
+  deleteLog,
+  getLog,
+  getLogs,
+  updateLog,
+} from "../controllers/logController";
 import authenticate from "../middlewares/auth";
 
 const logRouter = express.Router();
 
 logRouter.route("/").post(authenticate, createLog).get(getLogs);
 
-logRouter.route("/:id").get(getLog)
+logRouter
+  .route("/:id")
+  .get(getLog)
+  .put(authenticate, updateLog)
+  .delete(authenticate, deleteLog);
+  
 export default logRouter;
