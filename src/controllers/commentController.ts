eef7e63d4826f591794bd8c008d80c61770303b6
jjeankim <miyukikj35@gmail.com>
2025-07-02 08:31:38 +0900

    Feat: 댓글 목록 가져오기 기능 구현

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,4 +1,4 @@
-import type { Response } from "express";
+import type { RequestHandler, Response } from "express";
 import prisma from "../lib/prisma";
 import type { UserRequest } from "../types/expressUserRequest";
 
@@ -8,7 +8,7 @@ export const createComment = async (req: UserRequest, res: Response) => {
     return;
   }
 
-  const logId = Number(req.params.id);
+  const logId = Number(req.params.logId);
   const { content } = req.body;
   try {
     const comment = await prisma.comment.create({
@@ -21,6 +21,21 @@ export const createComment = async (req: UserRequest, res: Response) => {
     res.status(201).json({ message: "ok", data: comment });
   } catch (error) {
     console.error("댓글 작성 중 에러:", error);
+    res.status(500).json({ message: "서버 내부 오류가 발생했습니다." });
+  }
+};
+
+export const getComments: RequestHandler = async (req, res) => {
+  const logId = Number(req.params.logId);
+  try {
+    const comments = await prisma.comment.findMany({
+      where: { logId },
+    });
+    res
+      .status(200)
+      .json({ message: "댓글 목록 가져오기 성공", data: comments });
+  } catch (error) {
+    console.error("댓글 목록 가져오기 중 에러:", error);
     res.status(500).json({ message: "서버 내부 오류가 발생했습니다." });
   }
 };
