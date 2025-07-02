1409613e8cb63872238322cfcda7f59836c8a3f7
김진 <miyukikj35@gamil.com>
2025-07-02 12:25:17 +0900

    Feat: 회원가입 스키마에 정규표현식 추가(대문자,특수문자)

--- src/validators/authSchema.ts
+++ src/validators/authSchema.ts
@@ -9,7 +9,9 @@ export const registerSchema = z.object({
     .string({ required_error: "비밀번호는 필수 입력 항목입니다." })
     .trim()
     .min(8, "비밀번호는 최소 8자리 이상이어야 합니다.")
-    .max(20, "비밀번호는 최대 20자 이하여야 합니다."),
+    .max(20, "비밀번호는 최대 20자 이하여야 합니다.")
+    .regex(/[A-Z]/,"비밀번호는 최소 하나의 대문자가 포함되어야 합니다.")
+    .regex(/[^A-Za-z0-9]/,"비밀번호는 최소 하나의 특수문자가 포함되어야 합니다."),
   username: z
     .string({ required_error: "이름은 필수 입력 항목입니다." })
     .trim()
