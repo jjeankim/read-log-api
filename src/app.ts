c67f61eab6990e73a59a53c0b10d6c000ed7dcdd
김진 <miyukikj35@gamil.com>
2025-07-03 10:04:42 +0900

    Feat:없는페이지 및 서버 에러 처리 기능 구현

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import userRouter from "./routes/userRouter";
 import authRouter from "./routes/authRouter";
 import cookieParser from "cookie-parser";
@@ -10,12 +10,11 @@ dotenv.config();
 import cors from "cors";
 import { uploadPath } from "./middlewares/upload";
 
-
 const app = express();
 
 const corsOptions = {
   origin: ["http://localhost:3000"],
-  credentials:true,
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -28,7 +27,16 @@ app.use("/logs", logRouter);
 app.use("/logs/:logId/comments", commentRouter);
 app.use("/uploads", express.static(uploadPath));
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(404)
+    .json({ message: "요청하신 리소스를 찾을 수 없습니다.", status: "Fail" });
+});
 
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error.stack());
+  res.status(500).json(`sever error ${error.stack}, status: "Error`);
+});
 
 app.listen(3000, () => {
   console.log("Server started!");
